940eb459cb1755a76be7cdea5b68e7250beca038
msteslov <msteslov@edu.hse.ru>
2026-05-23 23:40:49 +0300

    Implement modular exponentiation

--- cryptalgs/math/modular.py
+++ cryptalgs/math/modular.py
@@ -5,3 +5,24 @@ Planned:
 - extended_gcd
 - modular_inverse
 """
+
+def modular_pow(base: int, exponent: int, modulus: int) -> int:
+    if modulus <= 0:
+        raise ValueError("modulus must be positive")
+    if exponent < 0:
+        raise ValueError("exponent must be non-negative")
+
+    r = 1
+    while exponent != 0:
+        if exponent % 2:
+            r = r * base % modulus
+        exponent //= 2
+        base = base * base % modulus
+
+    return r
+
+def extended_gcd(a: int, b: int) -> tuple[int, int, int]:
+    pass
+
+def modular_inverse(a: int, modulus: int) -> int:
+    pass
