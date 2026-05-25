ccc993453ff6e519e56cf72f95182b2e8914d2f5
msteslov <msteslov@edu.hse.ru>
2026-05-25 12:16:29 +0300

    Add GFp class skeleton

--- cryptalgs/math/gf.py
+++ cryptalgs/math/gf.py
@@ -1,1 +1,32 @@
-"""Arithmetic in finite fields GF(p)."""
+class GFp:
+
+    def __init__(self, value: int, p: int):
+        pass
+
+
+    def __add__(self, other: "GFp") -> "GFp":
+        pass
+
+    def __sub__(self, other: "GFp") -> "GFp":
+        pass
+
+    def __mul__(self, other: "GFp") -> "GFp":
+        pass
+
+    def __truediv__(self, other: "GFp") -> "GFp":
+        pass
+
+    def __pow__(self, other: "GFp") -> "GFp":
+        pass
+
+    def __neg__(self, other: "GFp") -> "GFp":
+        pass
+
+    def inverse(self, other: "GFp") -> "GFp":
+        pass
+
+    def __eq__(self, other: object) -> bool:
+        pass
+
+    def __repr__(self) -> str:
+        pass
